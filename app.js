a7f428e4a56ce7891d86eb03d85f7727e909c7de
Dima215 <dimaogar215@gmail.com>
2021-07-14 22:18:31 +0300

    work with js: add cart, modal, hearts, and ..1/2scroll

---
+++ app.js
@@ -0,0 +1,66 @@
+// add to cart
+
+let productsCountEl = document.getElementById("product-count");
+//console.log(productsCountEl);
+let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
+//console.log(addToCartBtns);
+
+
+for(let i = 0; i < addToCartBtns.length; i++) {
+    addToCartBtns[i].addEventListener("click",function() {
+        productsCountEl.textContent = +productsCountEl.textContent + 1;
+    })
+};
+
+// modal
+
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".btn-close");
+let moreDetailsBtns = document.querySelectorAll(".btn-more-details");
+
+moreDetailsBtns.forEach(btn => {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+    btn.addEventListener("click", openModal)
+});
+
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+};
+function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+};
+
+modal.addEventListener("click", function (e) {
+    if (e.target === modal) {
+        closeModal()
+    }
+});
+
+//hearts
+
+let heartsBtns = document.querySelectorAll(".like");
+//console.log(heartsBtns);
+
+for(let i = 0; i < addToCartBtns.length; i++) {
+    heartsBtns[i].addEventListener("click",function() {
+        heartsBtns[i].classList.toggle("like_click_js");
+    })
+};
+
+
+//scroll
+
+let scrollHeight = Math.max(
+  document.body.scrollHeight, document.documentElement.scrollHeight,
+  document.body.offsetHeight, document.documentElement.offsetHeight,
+  document.body.clientHeight, document.documentElement.clientHeight
+);
+console.log(scrollHeight / 2);
+console.log(window.scrollY);
+
+if (window.scrollY > scrollHeight / 2) {
+    openModal();
+};
